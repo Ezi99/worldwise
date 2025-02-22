681f87982a140110a3b68ce5711ab100a0534170
Ezi99 <ismaeljubranja@mta.ac.il>
2025-02-22 16:25:03 +0200

    Add: deleting a city

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -72,9 +72,37 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      if (!res.ok) {
+        throw new Error(
+          `HTTP error! Status: ${res.status}, StatusText: ${res.statusText}`
+        );
+      }
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
