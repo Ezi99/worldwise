48c7416fd2612e21224eefb9a6c389f495dcbe8b
Ezi99 <ismaeljubranja@mta.ac.il>
2025-02-23 22:49:06 +0200

    Fix: calling navigate outside useEffect

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,13 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
 
 function ProtectedRoutes({ children }) {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    navigate("/");
-  }
+  useEffect(
+    function () {
+      if (!isAuthenticated) {
+        navigate("/");
+      }
+    },
+    [isAuthenticated, navigate]
+  );
 
   return isAuthenticated ? children : null;
 }
